aae9e07060d22d45595160016b96d7ba5a12a9f4
安之111 <11119537+anzhi-111@user.noreply.gitee.com>
2022-08-17 17:25:24 +0800

    关注

---
+++ pages/user/edit/edit.vue
@@ -0,0 +1,116 @@
+<template>
+	<view>
+		<view class="bar">
+		</view>
+		<navigation :title="title"></navigation>
+		<view class="headportrait">
+			<image src="@/static/home/a.pic.jpg" mode="aspectFill" class="headportraitimg"></image>
+			更换头像
+			
+		</view>
+		<view v-for="(item,index) in  prices" :key="index">		
+		
+		<view class="name">
+			<view class="nickname">
+				{{item.text}}
+			</view>
+			<view class="modifyname">
+			<input type="text"  v-model="value" maxlength="10"  class="modifyinput" v-if="index==0?true:false">
+			<view class="modifyinput" v-else>
+				546546g
+			</view>
+			<image src="@/static/settings/jinnru.png" class="getintoimg"></image>		
+			</view>			
+		</view>
+		
+		</view>
+	</view>
+</template>
+
+<script>
+	import navigation from "@/components/navigation.vue"
+	export default {
+		data() {
+			return {
+				title: "编辑个人资料",
+				value:"胡编辑个ß",
+				prices: [{
+						text: "昵称"
+					},
+					{
+						text: "ID"
+					}, {
+						text: "性别"
+					}, {
+						text: "标签"
+					}, {
+						text: "星座"
+					}, 
+				
+				],
+				
+				
+			}
+		},
+		components: {
+			navigation,
+			
+		},
+		methods: {
+			a(){
+				console.log("as",this.text);
+			}
+		}
+	}
+</script>
+
+<style>
+.a{
+	color: white;
+}
+.headportrait{
+	width: 168rpx;
+	font-size: 28rpx;
+	color: rgba(255, 255, 255, 0.6);
+	margin: 40rpx auto;
+	text-align: center;
+	
+}
+.headportraitimg{
+	width: 168rpx;
+	height: 168rpx;
+	border-radius: 100rpx;
+	margin: 10rpx auto;
+	
+}
+.name{
+	margin-top: 60rpx;
+	width: 704rpx;
+	margin-left: 22rpx;
+	display: flex;
+	justify-content: space-between;
+}
+.nickname{
+	font-size: 28rpx;
+	color: rgba(255, 255, 255, 0.8);
+}
+.modifyname{
+	display: flex;
+	
+	font-size: 28rpx;
+	color: rgba(255, 255, 255, 0.5);
+}
+.modifyinput{
+	
+	font-size: 28rpx;
+	text-align: right;
+	color: rgba(255, 255, 255, 0.5);
+}
+
+.getintoimg{
+	width: 20rpx;
+	height: 20rpx;
+	margin-top: 10rpx;
+	margin-left: 20rpx;
+}
+</style>
--- pages/user/purchase/purchase.vue
+++ pages/user/purchase/purchase.vue
@@ -15,68 +15,68 @@
 			        <swiper class="swiper-content" :style="{height:swiperheight+'rpx'}" :current="tabIndex"
 				@change="swiperChange">
 				            <swiper-item class="itemswiper">
-					<scroll-view scroll-y="true" class="swiper-scroll" >
+					<scroll-view scroll-y="true" class="swiper-scroll">
 						<view v-for="(item ,index) in  prices" :key="index">
-							
-						
-						     <view class="viewat" @tap="order">
-							<view class="picture">
-								<image src="@/static/home/b.pic.jpg" mode="aspectFill" class="pictureimg"></image>
-							</view>
-							<view class="synopsis">
-								<view class="synopsistext">
-									[人气爆款]329，价值599元高等配置 无限畅玩
-									通宵，饮料零食全提供~[人气爆款]329，价值599元高等配置 无限畅玩
-									通宵，饮料零食全提供~
+
+
+							     <view class="viewat">
+								<view class="picture" @tap="order">
+									<image src="@/static/home/b.pic.jpg" mode="aspectFill" class="pictureimg"></image>
 								</view>
-								<view class="synopsisunmber">
-									<view class="effective">
-										<view class="price">
-											￥329
+								<view class="synopsis">
+									<view class="synopsistext">
+										[人气爆款]329，价值599元高等配置 无限畅玩
+										通宵，饮料零食全提供~[人气爆款]329，价值599元高等配置 无限畅玩
+										通宵，饮料零食全提供~
+									</view>
+									<view class="synopsisunmber">
+										<view class="effective">
+											<view class="price">
+												￥329
+											</view>
+											<view class="date">
+												{{item.text}}
+											</view>
 										</view>
-										<view class="date">
-											{{item.text}}
+										<view class="code" @tap="remove">
+											<image src="@/static/user/erweima.png" class="codeimg"></image>
 										</view>
 									</view>
-									<view class="code">
-										<image src="@/static/user/erweima.png" class="codeimg"></image>
-									</view>
 								</view>
 							</view>
-						</view>
 						</view>
 					</scroll-view>            
 				</swiper-item>
 				<swiper-item class="itemswiper">
-					<scroll-view scroll-y="true" class="swiper-scroll" >
+					<scroll-view scroll-y="true" class="swiper-scroll">
 						<view v-for="(item ,index) in  prices" :key="index">
-							
-						
-						     <view class="viewat" @tap="order">
-							<view class="picture">
-								<image src="@/static/home/b.pic.jpg" mode="aspectFill" class="pictureimg"></image>
-							</view>
-							<view class="synopsis">
-								<view class="synopsistext">
-									[人气爆款]329，价值599元高等配置 无限畅玩
-									通宵，饮料零食全提供~[人气爆款]329，价值599元高等配置 无限畅玩
-									通宵，饮料零食全提供~
+
+
+							     <view class="viewat" @tap="order">
+								<view class="picture">
+									<image src="@/static/home/b.pic.jpg" mode="aspectFill" class="pictureimg"></image>
 								</view>
-								<view class="synopsisunmber">
-									<view class="effective">
-										<view class="price">
-											￥329
+								<view class="synopsis">
+									<view class="synopsistext">
+										[人气爆款]329，价值599元高等配置 无限畅玩
+										通宵，饮料零食全提供~[人气爆款]329，价值599元高等配置 无限畅玩
+										通宵，饮料零食全提供~
+									</view>
+									<view class="synopsisunmber">
+										<view class="effective">
+											<view class="price">
+												￥329
+											</view>
+											<view class="date">
+												{{item.text}}
+											</view>
 										</view>
-										<view class="date">
-											{{item.text}}
+										<view class="codes">
+											<image src="@/static/user/shhixiao.png" class="codesimg"></image>
 										</view>
 									</view>
-									<view class="codes">
-										<image src="@/static/user/shhixiao.png" class="codesimg"></image>
-									</view>
 								</view>
 							</view>
-						</view>
 						</view>
 					</scroll-view>            
 				</swiper-item>
@@ -84,8 +84,15 @@
 			</swiper>
 			    
 		</view>
+		<!-- 弹框 -->
+		<view class="remove" v-if="show" @tap="cancel">
+			1
+		</view>
+		<!-- 遮罩层 -->
+		<view :class="show==true? 'actives': 'shelter' ">
 
 
+		</view>
 
 
 	</view>
@@ -98,6 +105,7 @@
 		data() {
 			return {
 				title: "我购买的",
+				show: false,
 				swiperheight: 0,
 				tabIndex: 0, // 选中的
 				tabBars: [{
@@ -117,14 +125,14 @@
 						text: "好来玩电竞网咖"
 					}, {
 						text: "¥546"
-					},{
+					}, {
 						text: "¥546"
-					},{
+					}, {
 						text: "¥546"
-					},{
+					}, {
 						text: "¥546"
 					},
-				
+
 				],
 
 
@@ -156,11 +164,18 @@
 				var _this = this
 				_this.tabIndex = e.detail.current
 			},
-			order(){
+			order() {
 				uni.navigateTo({
-					url:'/pages/user/purchase/order'
+					url: '/pages/user/purchase/order'
 				})
-			}
+			},
+			remove() {
+				this.show = true
+
+			},
+			cancel() {
+				this.show = false
+			},
 
 
 		}
@@ -170,7 +185,7 @@
 <style>
 	.swiper-scroll {
 		height: 100%;
-	
+
 	}
 
 	.content {
@@ -223,13 +238,15 @@
 
 	.synopsistext {
 
-/* height: 68rpx; */
-overflow: hidden;
+		/* height: 68rpx; */
+		overflow: hidden;
 
-text-overflow: ellipsis;
-display: -webkit-box; /** 对象作为伸缩盒子模型显示 **/
--webkit-box-orient: vertical; /** 设置或检索伸缩盒对象的子元素的排列方式 **/
--webkit-line-clamp: 2; 
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		/** 对象作为伸缩盒子模型显示 **/
+		-webkit-box-orient: vertical;
+		/** 设置或检索伸缩盒对象的子元素的排列方式 **/
+		-webkit-line-clamp: 2;
 		font-size: 24rpx;
 		color: rgba(255, 255, 255, 0.8);
 	}
@@ -262,14 +279,43 @@ display: -webkit-box; /** 对象作为伸缩盒子模型显示 **/
 		width: 60rpx;
 		height: 60rpx;
 	}
-	
+
 	.codes {
 		margin-top: 30rpx;
 		width: 120rpx;
 	}
-	
+
 	.codesimg {
 		width: 112rpx;
 		height: 92rpx;
+	}
+
+	.actives {
+		background-color: rgba(0, 0, 0, 0.5);
+		z-index: 2;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+	}
+
+	.shelter {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		z-index: -2;
+
+	}
+
+	.remove {
+		position: absolute;
+		left: 126rpx;
+		top: 500rpx;
+		width: 500rpx;
+		height: 500rpx;
+		background-color: #29253C;
+		border-radius: 20rpx;
+		z-index: 4;
 	}
 </style>
